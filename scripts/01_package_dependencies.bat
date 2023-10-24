6970528547100c709320b7006ed29354b35b59ea
Justus van den Berg <jfwberg@gmail.com>
2023-10-24 09:49:03 +0100

    updated permission set updates version updated dependencies

--- scripts/01_package_dependencies.bat
+++ scripts/01_package_dependencies.bat
@@ -1,5 +1,12 @@
-REM Managed package - Lightweight - Apex Unit Test Util v2@2.2.0-2
-sf package install --package 04tP30000007Ez7IAE -w 30
+REM --------------------------------------------------------
+REM MANGED DEPENDENCIES (PICK EITHER MANAGED OR UNLOCKED)  -
+REM --------------------------------------------------------
+REM Managed package - Lightweight - Apex Unit Test Util v2@2.3.0-1
+sf package install --package 04tP30000007oePIAQ -w 30
 
-REM Lightweight - Apex Unit Test Util v2 (Unlocked)@2.2.0-2
-sf package install --package 04tP30000007F3xIAE -w 30
+
+REM --------------------------------------------------------
+REM UNLOCKED DEPENDENCIES (PICK EITHER MANAGED OR UNLOCKED)-
+REM --------------------------------------------------------
+REM Unlocked package - Lightweight - Apex Unit Test Util v2 (Unlocked)@2.3.0-1
+sf package install --package 04tP30000007og1IAA -w 30
--- scripts/03_package_create_unlocked.bat
+++ scripts/03_package_create_unlocked.bat
@@ -11,7 +11,7 @@ SET packagePath=force-app/package
 
 REM Package Config
 SET packageId=0HoP30000000085KAA
-SET packageVersionId=04tP30000007GXtIAM
+SET packageVersionId=04tP30000007tB3IAI
 
 REM Create package
 sf package create --name "%packageName%" --description "%packageDescription%" --package-type "%packageType%" --path "%packagePath%" --target-dev-hub %devHub%
